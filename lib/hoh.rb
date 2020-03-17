78b60c213f775dc5b22f8b217e7d6da8c4cf0dc9
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-17 17:06:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,27 @@
 # }
 
 def naming_system {
-  :label = > "Kingdom"
+  :label = > "Kingdom",
+  :sub_category => {
+      :label => "Phylum",
+      :sub_category => {
+        :label => "Class",
+           :sub_category {
+            :label => "Order",
+            :label => "Family",
+      :sub_category => {
+            :label => "Genus",
+            :label => "Species",
+              :sub_category: nil
+              
+          }
+            
+            
+          }
+          }
+      }
+    }
+ }
   
   # Remember:
   #  Kingdom
